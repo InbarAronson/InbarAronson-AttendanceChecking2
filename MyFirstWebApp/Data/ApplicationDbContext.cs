6bac10c843ee307d8e16a410090570c07ad5835d
Steve Gillott <steve@stevegillott.co.uk>
2023-03-27 16:35:07 +0200

    Student CRUD working

--- MyFirstWebApp/Data/ApplicationDbContext.cs
+++ MyFirstWebApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyFirstWebApp.Models;
 
 namespace MyFirstWebApp.Data
 {
@@ -9,5 +10,6 @@ namespace MyFirstWebApp.Data
             : base(options)
         {
         }
+        public DbSet<MyFirstWebApp.Models.Student> Student { get; set; }
     }
 }

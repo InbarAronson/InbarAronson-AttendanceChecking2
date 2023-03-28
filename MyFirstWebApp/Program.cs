ec981fa3d2328c094ad25e09ebb9dec3051df01c
Steve Gillott <steve@stevegillott.co.uk>
2023-03-28 12:00:40 +0200

    Seeded Data

--- MyFirstWebApp/Program.cs
+++ MyFirstWebApp/Program.cs
@@ -34,7 +34,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDbContext>();
     context.Database.EnsureCreated();
-    // DbInitializer.Initialize(context);
+    DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();

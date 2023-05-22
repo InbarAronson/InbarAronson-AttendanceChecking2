141327a471b4d9f53fbe126730fea94e1af46e96
Inbar Aronson <iaronson@is-ulm.de>
2023-05-22 09:41:16 +0200

    Working project

--- MyFirstWebApp/Models/RFID.cs
+++ MyFirstWebApp/Models/RFID.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyFirstWebApp.Models
 {
     public class RFIDCard
     {
+        [ForeignKey ("Student")]
         public int StudentID { get; set; }
         [Key]
         public int RFID { get; set; }
